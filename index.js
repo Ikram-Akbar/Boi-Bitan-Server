b38745b69bbf73e3667ae38bcc35fc9e6244812d
Ikram Akbar <ikramakbar71@gmail.com>
2025-06-08 01:32:01 +0600

    Api / delete one by Id

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ async function run() {
       res.status(200).send(result);
     });
 
+    //delete a book by id 
+    app.delete("/api/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await booksCollection.deleteOne(query);
+      res.send(result);
+    });
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
